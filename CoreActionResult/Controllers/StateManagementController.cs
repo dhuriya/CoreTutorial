ed1ee414be419488ba2fd0a886294145f594cf28
Deepu <deepukasyap143@gmail.com>
2025-06-12 22:17:05 +0530

    bbb

---
+++ CoreActionResult/Controllers/StateManagementController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreActionResult.Controllers
+{
+    public class StateManagementController : Controller
+    {
+        const string CookieUserId = "UserId";
+        const string CookieName = "Deepu";
+        public IActionResult Index()
+        {
+            CookieOptions options = new CookieOptions()
+            {
+
+            };
+            return View();
+        }
+    }
+}
