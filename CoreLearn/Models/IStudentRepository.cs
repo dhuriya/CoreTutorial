d10ee22d53a034837b48f123cdb42fafc2e9e051
deepu <deepukasyap143@gamil.com>
2026-05-25 14:24:38 +0530

    core

---
+++ CoreLearn/Models/IStudentRepository.cs
@@ -0,0 +1,1 @@
+Student GetStudentById(int StudentId);
---
+++ CoreLearn/Models/StudentRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreLearn.Models;
+
+namespace CoreLearn.Models
+{
+    public class StudentRepository:IStudentRepository
+    {
+        public List<Student>
+    }
+}
