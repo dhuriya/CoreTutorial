22d23f042cc5e3d3eb9886ce726fa44757558a48
deepu <deepukasyap143@gamil.com>
2026-05-28 17:06:36 +0530

    28/05/2026

---
+++ CoreLearn/Models/StudentDetailsViewModel.cs
@@ -0,0 +1,10 @@
+namespace CoreLearn.Models
+{
+    public class StudentDetailsViewModel
+    {
+        public Student? student { get; set; }
+        public Address? Address { get; set; }
+        public string? Title { get; set; }
+        public string? Header { get; set; }
+    }
+}
