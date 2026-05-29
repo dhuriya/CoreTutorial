b0de50fd04f7a9e2c96ae484b573835228a96c78
deepu <deepukasyap143@gamil.com>
2026-05-29 16:48:20 +0530

    29/05/2026

---
+++ CoreLearn/Models/Feedback.cs
@@ -0,0 +1,11 @@
+namespace CoreLearn.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Comment { get; set; }
+
+    }
+}
