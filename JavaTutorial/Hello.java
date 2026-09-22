7144708370eeb912aae90dd02e53c5786d66c931
deepu <deepukasyap143@gamil.com>
2026-09-22 18:01:59 +0530

    java start

---
+++ JavaTutorial/Hello.java
@@ -0,0 +1,39 @@
+
+package JavaTutorial;
+class Hello 
+{
+    public static void main(String args[])
+    {
+        // int num1 = 3;
+        // int num2 = 5;
+        // byte by = 127; // throw if i put greterthan 127
+        // short sh = 558;
+        // long l = 5854l;
+
+        // float f = 5.8f;
+        // double d = 5.8;
+        // char ch = 'k';
+        // boolean b = true; // literls
+        // int result = num1 + num2;
+
+        //byte bt = 127;
+        //int aa = bt;
+        // int aa = 257;
+        // byte k = (byte) aa;//TypeCasting
+        // float f = 5.6f;
+        // int  t = (int)f; // typecasting
+        // byte a = 10;
+        // byte b = 30;
+        // System.out.print(a * b);
+
+        //---------------------
+        // Opreators
+        //--------------------
+        int num1 = 7;
+        //int num2 = 5;
+        //int result = num1 % num2;
+        //num1 = num1 + 2;
+        num1 += 2;
+        System.out.println(num1);
+    }
+}
